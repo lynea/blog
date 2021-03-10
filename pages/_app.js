ea75aa2ca3ac27bc6fa6e2239c43868bbb986000
Rene van Dijk <renevandijk3@hotmail.com>
2021-03-10 22:32:00 +0100

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
-import '@/styles/index.css'
-
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
